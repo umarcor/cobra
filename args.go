e91a2e793fea935f61ab5623860f706eab68b72f
umarcor <unai.martinezcorral@ehu.eus>
2019-03-21 19:20:52 +0100

    rename 'honour' to 'honor'

--- args.go
+++ args.go
@@ -92,14 +92,14 @@ func RangeArgs(min int, max int) PositionalArgs {
 // ExactValidArgs returns an error if there are not exactly N positional args OR
 // there are any positional args that are not in the `ValidArgs` field of `Command`
 //
-// Deprecated: now `ExactArgs` honours `ValidArgs`, when defined and not empty
+// Deprecated: now `ExactArgs` honors `ValidArgs`, when defined and not empty
 func ExactValidArgs(n int) PositionalArgs {
 	return ExactArgs(n)
 }
 
 // OnlyValidArgs returns an error if any args are not in the list of `ValidArgs`.
 //
-// Deprecated: now `ArbitraryArgs` honours `ValidArgs`, when defined and not empty
+// Deprecated: now `ArbitraryArgs` honors `ValidArgs`, when defined and not empty
 func OnlyValidArgs(cmd *Command, args []string) error {
 	return ArbitraryArgs(cmd, args)
 }
